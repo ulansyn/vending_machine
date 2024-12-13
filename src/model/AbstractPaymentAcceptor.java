4a3c1ce5d87d1b00bb3e22480e1fcd41afdfff7f
ulansyn <ullansyn@gmail.com>
2024-12-13 17:42:11 +0600

    created new abstact class for general amount for payment methods

--- src/model/AbstractPaymentAcceptor.java
+++ src/model/AbstractPaymentAcceptor.java
@@ -1,4 +1,9 @@
 package model;
 
-public class AbstractPaymentAcceptor {
+public abstract class AbstractPaymentAcceptor implements PaymentAcceptor {
+    public static int totalAmount = 0; //общий счетчик в автомате
+
+    public static int getTotalAmount() {
+        return totalAmount;
+    }
 }
