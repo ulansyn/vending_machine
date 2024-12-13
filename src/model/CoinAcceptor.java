1346ba69921413e2788388c88ca5259e48c12896
ulansyn <ullansyn@gmail.com>
2024-12-13 17:14:40 +0600

    Created new general interface for payment methods

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,16 +1,22 @@
 package model;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements PaymentAcceptor {
     private int amount;
 
     public CoinAcceptor(int amount) {
         this.amount = amount;
     }
 
+    @Override
+    public boolean authorize() {
+        return false;
+    }
+
     public int getAmount() {
         return amount;
     }
 
+    @Override
     public void setAmount(int amount) {
         this.amount = amount;
     }
